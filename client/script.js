f79764004a99bc1e07431f6a17deef65bb058608
Cruz <cruz_ch96@outlook.com>
2023-01-18 00:45:19 +0800

    update server url call

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from node server
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chatgpt-clone-server-gyrt.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
