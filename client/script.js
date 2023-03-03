6d19f0a258419e237814805f8683ca57098a5ecd
Cruz <cruz_ch96@outlook.com>
2023-03-03 17:54:59 +0800

    Added Access Authorization, slight CSS changes

--- client/script.js
+++ client/script.js
@@ -1,5 +1,6 @@
 import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
+import CryptoJS from "crypto-js";
 
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
@@ -41,6 +42,17 @@ function generateUniqueId() {
   return `id-${timestamp}-${hexadecimalString}`;
 }
 
+function validateUser(value) {
+  return `
+      <div class="wrapper">
+          <div class="message authorizeDiv">
+          <div class="authorizeText">Please contact Project Owner for Authorization Key to proceed</div>
+            <input type="text" id="authorizeMessage" />
+          </div>
+      </div>
+  `;
+}
+
 function chatStripe(isAi, value, uniqueId) {
   return `
       <div class="wrapper ${isAi && "ai"}">
@@ -57,14 +69,74 @@ function chatStripe(isAi, value, uniqueId) {
   `;
 }
 
+const secretKey = import.meta.env.VITE_SECRET_KEY;
+const authKey = import.meta.env.VITE_AUTH_KEY;
+// const encryptedChatKey = CryptoJS.AES.encrypt(authKey, secretKey).toString();
+
+const sessionChatKey = JSON.parse(sessionStorage.getItem("chatKey"));
+const decryptedChatValue = sessionChatKey
+  ? CryptoJS.AES.decrypt(sessionChatKey.value, secretKey).toString(
+      CryptoJS.enc.Utf8
+    )
+  : "";
+
+const authorizedBrowser = authKey === decryptedChatValue;
+
+console.log(authorizedBrowser);
+
 const handleSubmit = async (e) => {
   e.preventDefault();
-
   const data = new FormData(form);
+  form.reset();
+
+  //check timestamp for key expiry
+  if (sessionChatKey && Date.now() > sessionChatKey.expiry) {
+    // Remove the chatKey from sessionStorage
+    sessionStorage.removeItem("chatKey");
+    console.log("removed chatkey");
+    // Reload the page
+    location.reload();
+    alert("Access Expired, Please Try Again");
+    return;
+  }
+
+  if (!authorizedBrowser) {
+    chatContainer.innerHTML += validateUser("test");
+    let authorizeMessage = chatContainer.querySelector("#authorizeMessage");
+    authorizeMessage.addEventListener("keyup", (e) => {
+      if (e.keyCode === 13 && e.target.value) {
+        // console.log(e.target.value);
+        const keyVal = e.target.value;
+
+        // console.log("decryptedChatKey ", decryptedChatKey);
+        // console.log("authKey ", authKey);
+        // console.log("secretKey ", secretKey);
+
+        if (keyVal === authKey) {
+          const encryptedKey = CryptoJS.AES.encrypt(
+            keyVal,
+            secretKey
+          ).toString(); //encrypt key in client browser
+
+          sessionStorage.setItem(
+            "chatKey",
+            JSON.stringify({
+              value: encryptedKey,
+              expiry: Date.now() + 600000, // 10 minutes in milliseconds
+            })
+          );
+          alert("Access Granted! Expired in 10 minutes");
+
+          window.location.reload(); //force reload client browser to capture authorization
+        } else {
+          alert("Unauthorized Access!");
+        }
+      }
+    });
+    return;
+  }
 
   chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
-
-  form.reset();
 
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
@@ -76,7 +148,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from node server
 
-  const response = await fetch("https://chatgpt-clone-server-gyrt.onrender.com/", {
+  const response = await fetch(import.meta.env.VITE_FETCH_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -96,7 +168,7 @@ const handleSubmit = async (e) => {
   } else {
     const err = await response.text();
     messageDiv.innerHTML = "Error occurs";
-    alert(err);
+    // alert(err);
   }
 };
 
